f102734369d07a0ac1a538896f724ddbd6556d8b
ane-noelle <anenoelle60@gmail.com>
2026-03-03 10:50:33 -0300

    dia3

--- ToDoPlatform/Program.cs
+++ ToDoPlatform/Program.cs
@@ -1,6 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using ToDoPlatform.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//Servico de conexao com o banco de dados
+string conexao = builder.Configuration 
+.GetConnectionString("Conexao");
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseMySQL(conexao)
+);
+
+//Serviço de configuracao de gestao de usuarios
+
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
