655f00e9349cb7d829dcc4b9b89610de7a153404
ane-noelle <anenoelle60@gmail.com>
2026-04-07 10:50:43 -0300

    acabadocomerro

--- ToDoPlatform/Controllers/AccountController.cs
+++ ToDoPlatform/Controllers/AccountController.cs
@@ -53,6 +53,33 @@ public class AccountController : Controller
         return RedirectToAction("Login", "Account");
     }
 
-    public IActionResult Register() => View();
+    [HttpGet]
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Register(RegisterVM registerVM)
+    {
+        if (ModelState.IsValid && registerVM.Terms)
+        {
+            var result = await _userService.Register(registerVM);
+
+            if (result.Count == 0)
+                TempData["Success"] = "Conta criada com sucesso! Redirecionando...";
+            else
+            {
+                foreach (var error in result)
+                    TempData["Failure"] += error + "\n";
+            }
+        }
+        else
+            TempData["Failure"] = "Dados inválidos. Verifique os campos preenchidos.";
+
+        return View(registerVM);
+    }
+
     public IActionResult Profile() => View();
 }
