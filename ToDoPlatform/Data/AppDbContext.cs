c4b7791ed0c8f0297b46795486d199965f9b9de7
ane-noelle <anenoelle60@gmail.com>
2026-04-09 10:45:02 -0300

    pronto

--- ToDoPlatform/Data/AppDbContext.cs
+++ ToDoPlatform/Data/AppDbContext.cs
@@ -5,31 +5,29 @@ using ToDoPlatform.Models;
 
 namespace ToDoPlatform.Data;
 
-    public class AppDbContext : IdentityDbContext<AppUser>
+public class AppDbContext : IdentityDbContext<AppUser>
+{
+    public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt)
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options)
-            : base(options)
-        {
-            
-        }
+    }
 
-        public DbSet<AppUser> AppUsers { get; set; }
-        public DbSet<ToDo> ToDos { get; set; }
+    public DbSet<AppUser> AppUsers { get; set; }
+    public DbSet<ToDo> ToDos { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
-        
-        AppDbSeed appDbSeed = new(builder);
 
+        new AppDbSeed(builder);
+
+        #region Configuração das Tabelas do Identity
         builder.Entity<AppUser>().ToTable("users");
         builder.Entity<IdentityRole>().ToTable("roles");
         builder.Entity<IdentityUserRole<string>>().ToTable("user_roles");
         builder.Entity<IdentityUserClaim<string>>().ToTable("user_claims");
         builder.Entity<IdentityUserToken<string>>().ToTable("user_tokens");
         builder.Entity<IdentityUserLogin<string>>().ToTable("user_logins");
-        builder.Entity<IdentityRoleClaim<string>>().ToTable("role_claims");  
-
+        builder.Entity<IdentityRoleClaim<string>>().ToTable("role_claims");
+        #endregion
     }
-    
 }
